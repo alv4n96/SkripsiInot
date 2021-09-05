0e72afe95663331cf46322208fe4ccb008dddb3f
alv4n96 <alvan.ghon@gmail.com>
2021-09-05 17:16:07 +0700

    menjalankan file main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Text('Test!'),
     );
   }
 }
