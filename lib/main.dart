e700852a7d97a42250ab7778587f3fb3875a381e
alv4n96 <alvan.ghon@gmail.com>
2021-09-05 14:43:11 +0700

    Memasukkan fungsi awal, (Material App), ke dalam main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+void main() {}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Text('Hello!'),
+    );
+  }
+}
